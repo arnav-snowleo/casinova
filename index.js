924cb21935e2591cdc433d540720517540c05356
arnav-snowleo <prakashsinghaarav@gmail.com>
2022-03-23 13:05:56 +0530

    age, display cards and sum

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-alert("Play at your own risk");
+let age = prompt("enter your age");
+
+if (age < 22) {
+    alert("Not allowedd to enter");
+} else {
+    alert("Entry allowed");
+}
+
+let firstcard = Math.floor(Math.random() * (12 - 2) + 2);
+let secondcard = Math.floor(Math.random() * (12 - 2) + 2);
+
+// let firstcard = 11;
+// let secondcard = 10;
+let hasBlackJack = false;
+let isOutOfGame = false;
+
+let sum = firstcard + secondcard;
+let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.querySelector("#sum-el");
+let cardsEl = document.querySelector(".cards-el");
+
+function checkSum() {
+    cardsEl.textContent = "Cards: " + firstcard + ", " + secondcard;
+    sumEl.textContent = "Sum: " + sum;
+    if (sum <= 20) {
+        message = "Do you want to draw a new card ?";
+    } else if (sum === 21) {
+        hasBlackJack = true;
+        message = "Wohoo! you got a blackJack!";
+    } else {
+        isOutOfGame = true;
+        message = "You are out of the game";
+    }
+
+    messageEl.textContent = message;
+    // alert(message);
+}
